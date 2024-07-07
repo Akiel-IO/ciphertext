28ff69e2470b784572897469a3dd426fd820fd6d
Akiel-IO <daniel.cs_@outlook.com>
2024-07-07 16:12:00 -0500

    start commit

---
+++ src/test/java/com/ciphertext/ciphertext/CiphertextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciphertext.ciphertext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiphertextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
